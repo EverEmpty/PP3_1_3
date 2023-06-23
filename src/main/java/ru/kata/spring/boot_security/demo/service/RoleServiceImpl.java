321235faa528da514764e0701237f71587eac997
EverEmpty <fairel95@gmail.com>
2023-06-23 20:47:56 +0300

    done

--- src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImpl.java
@@ -3,7 +3,6 @@ package ru.kata.spring.boot_security.demo.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.entity.Role;
-//import ru.kata.spring.boot_security.demo.repository.RoleDao;
 import ru.kata.spring.boot_security.demo.repository.RoleRepository;
 
 import java.util.List;
